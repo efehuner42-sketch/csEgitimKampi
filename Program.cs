0341e18d1a50d38baa09694a165a46eb7989b39f
KodYazıyorum59 <efehuner42@gmail.com>
2025-10-25 14:26:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿namespace Cs
+{
+    class Ders_2
+    {
+        static void Main()
+        {
+            #region Variables
+
+            #region doubleDegiskenler 
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            //double apple, potato, strawberry, orange, tomato;
+
+            //apple = 4.70;
+            //potato = 15.67;
+            //strawberry = 45.32;
+            //orange = 21;
+            //tomato = 14.12;
+
+            //Console.WriteLine("----- Fiyat Listesi -----");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Elma fiyatı: " + apple );
+            //Console.WriteLine("Patates fiyatı: " + potato);
+            //Console.WriteLine("Çilek fiyatı: " + strawberry);
+            //Console.WriteLine("Portakal fiyatı: " + orange);
+            //Console.WriteLine("Domates fiyatı: " + tomato);
+
+
+
+
+
+
+            #endregion
+
+
+            #region charDegiskenler
+
+            // char degiskenler "" ile değil '' ile tanımlanır.
+
+            //char symbol = 'a';
+            //Console.WriteLine(symbol);
+
+
+
+            #endregion
+
+
+
+
+            #endregion
+
+            #region klavyedenVeriGirisleri
+
+            #region stringDegiskenler
+
+            //string passengerName, passengerSurname, passengerId;
+
+            //Console.WriteLine("---- Huner Havayolları -----");
+
+            //Console.Write("Adınız: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Soyadınız: ");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("TC. Kimlik No: ");
+            //passengerId = Console.ReadLine();
+
+            //Console.WriteLine("Yolcu : " + passengerName + " " + passengerSurname);
+            //Console.WriteLine("TC. Kimlik No: " + passengerId);
+
+
+
+
+
+
+            #endregion
+
+            #region intDegiskenlerVeDonusumler
+
+            //int computer, tv, phone, tab;
+
+            //computer = 30000;
+            //tv = 25000;
+            //phone = 20000;
+            //tab = 15000;
+
+            //int computerCount, tvCount, phoneCount, tabCount;
+
+            //Console.Write("Aldıgınız bilgisayar adedini giriniz: ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Aldıgınız TV adedini giriniz: ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Aldıgınız telefon adedini giriniz: ");
+            //phoneCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Aldıgınız tablet adedini giriniz: ");
+            //tabCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice;
+            //totalPrice = (tv*tvCount) + (phone*phoneCount) + (tab*tabCount) + (computer*computerCount);
+
+            //Console.Write("Toplam ödemeniz gereken fiyat " + totalPrice + " " + "TL");
+
+
+            #endregion
+
+            #region ondalıklıSayıIslemleri
+
+            //double exam1, exam2, exam3;
+
+            //Console.Write("1.Sınav notunuzu giriniz: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("2.Sınav notunuzu giriniz: ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("3.Sınav notunuzu giriniz: ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //double average;
+            //average = (exam1 + exam2 +  exam3) / 3;
+            //Console.Write("Ortalamanız: " +  average);
+
+
+            #endregion
+
+            #region charDegiskenler
+
+            //char gender;
+            //Console.Write("Cinsiyetinizi (E/K) giriniz: ");
+            //gender = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Cinsiyetiniz: " + gender);
+
+            #endregion
+            #endregion
+        }
+    }
+}
